6259277928297e8e61ddacd280e1a84963417c03
taehee.rho <taeheerho@gmail.com>
2023-06-08 15:22:46 +0900

    #5 Chore: 무한스크롤 기능 구현 완료

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,56 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Header from '../layout/Header'
-import Footer from '../layout/Footer'
-import EventBanner from '../common/EventBanner'
-import Product from '../common/Product'
-import TopButton from '../common/TopButton';
-import Filter from '../common/Filter';
-import styled from 'styled-components';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
+import EventBanner from "../common/EventBanner";
+import Product from "../common/Product";
+import TopButton from "../common/TopButton";
+import Filter from "../common/Filter";
+import styled from "styled-components";
 
 function Home() {
+  const [page, setPage] = useState(1);
+  const [products, setProducts] = useState([]);
+  const [hasNext, setHasNext] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const [products, setProducts] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-
-    const fetchProducts = async () => {
-        try {
-            setProducts(null);
-            setLoading(true);
-            setError(null);
-
-            const response = await axios.get("https://openmarket.weniv.co.kr/products/");
-            setProducts(response.data.results)
-
-        } catch(e) {
-            setError(e);
-        }
-        setLoading(false);
+  //초기 데이터 가져오기
+  const fetchProducts = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await axios.get(
+        `https://openmarket.weniv.co.kr/products/?page=${page}`
+      );
+      console.log(response.data);
+      return response.data;
+    } catch (e) {
+      setError(e);
     }
+    setLoading(false);
+  };
 
+  // 다음 페이지가 있을 경우에만 데이터 가져오기
+  useEffect(() => {
+    if (hasNext) {
+      (async () => {
+        await fetchProducts().then((res) => {
+          setProducts((prevProducts) => [...prevProducts, ...res.results]);
+          setHasNext(page + 1 <= Math.ceil(res.count / 15));
+        });
+      })();
+    }
+  }, [page, hasNext]);
 
-    useEffect(() => {
-        fetchProducts();
-    }, []);
+  console.log(page, hasNext);
 
-    if(loading) return <div>로딩중</div>;
-    if(error) return <div>에러발생!</div>;
-    if(!products) return null;
+  // 스크롤 이벤트 핸들러 등록
+  const scrollHandle = () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
 
+    if (scrollHeight <= scrollTop + clientHeight && hasNext) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
 
-    return(
-        <>
-            <Header />
-            <EventBanner />
-            <Filter />
-            <Product productApi={products}/>
-            <Footer />
-            <TopButton />
-        </>
-    )
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandle);
+
+    return () => window.removeEventListener("scroll", scrollHandle);
+  }, [hasNext]);
+
+  return (
+    <>
+      <Header />
+      <EventBanner />
+      <Filter />
+      <Product productApi={products} />
+      <Footer />
+      <TopButton />
+    </>
+  );
+
+  // useEffect(() => {
+  //   setIsBottom(scrollHeight <= scrollTop + clientHeight);
+
+  //     window.addEventListener("scroll", scrollHandle);
+  // });
+
+  // const scrollHandle = () => {
+  //   const scrollHeight = document.documentElement.scrollHeight;
+  //   const scrollTop = document.documentElement.scrollTop;
+  //   const clientHeight = document.documentElement.clientHeight;
+
+  //   if (scrollHeight <= scrollTop + clientHeight) {
+  //     fetchProducts();
+  //   }
+  //   console.log(products);
+
+  // }
 }
-
 
 export default Home;
